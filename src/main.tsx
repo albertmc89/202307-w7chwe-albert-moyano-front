3119ed5769effe97144fc4c036ae552b111a3b60
Albert Colubi <albertmc89@gmail.com>
2023-08-25 18:16:45 +0200

    Add Header component styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import "@fontsource/nunito/400.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./components/App";
+import App from "./components/App/App";
 import { store } from "./store";
+import "./styles/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
