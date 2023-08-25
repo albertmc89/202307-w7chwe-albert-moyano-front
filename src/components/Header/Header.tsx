c4b25772e94099a397c090400817a145077ae772
Albert Colubi <albertmc89@gmail.com>
2023-08-25 18:15:24 +0200

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = (): React.ReactElement => {
+  return (
+    <header className="header">
+      <div className="title-container">
+        <h1 className="header-title">RoboMate</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
