033b740a579b915a2e186823a9a85153684ff1cd
Albert Colubi <albertmc89@gmail.com>
2023-08-27 01:19:01 +0200

    Create useRobotsApi with getRobot function to get robots from api

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,11 @@
-export interface Robot {
-  id: number;
+export interface ApiRobot {
+  _id: string;
   name: string;
   image: string;
   speed: number;
   endurance: number;
+}
+
+export interface Robot extends Omit<ApiRobot, "_id"> {
+  id: string;
 }
