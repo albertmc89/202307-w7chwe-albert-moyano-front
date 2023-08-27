2e46b09a81eb13c56ab435692def0a65ed1fb123
Albert Colubi <albertmc89@gmail.com>
2023-08-27 23:21:07 +0200

    Create Loading component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import NewRobotPage from "../../pages/NewRobotPage/NewRobotPage";
 import RobotsListPage from "../../pages/RobotsListPage/RobotsListPage";
+import { useAppSelector } from "../../store";
 import Header from "../Header/Header";
+import Loading from "../Loading/Loading";
 import "./App.css";
 
 const App = (): React.ReactElement => {
+  const isLoading = useAppSelector((state) => state.uiState.isLoading);
+
   return (
     <div className="container">
       <Header />
@@ -15,6 +19,7 @@ const App = (): React.ReactElement => {
           <Route path="/createrobot" element={<NewRobotPage />} />
         </Routes>
       </main>
+      {isLoading && <Loading />}
     </div>
   );
 };
