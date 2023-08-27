d251a7e7969d5aeb1d639cf4311f59b64479b60f
Albert Colubi <albertmc89@gmail.com>
2023-08-27 12:46:48 +0200

    Add useAppDispatch and useEffect functions to App

--- src/hooks/useRobotsApi.test.tsx
+++ src/hooks/useRobotsApi.test.tsx
@@ -4,13 +4,13 @@ import { server } from "../mocks/server";
 import { errorHandlers } from "./handlers";
 import useRobotsApi from "./useRobotsApi";
 
-describe("Given function getRobot from useRobotsApi custom hook", () => {
+describe("Given function getRobots from useRobotsApi custom hook", () => {
   describe("When the function is called", () => {
     test("Then you will recieve a list of robots", async () => {
       const { result } = renderHook(() => useRobotsApi());
-      const { getRobot } = result.current;
+      const { getRobots } = result.current;
 
-      const robots = await getRobot();
+      const robots = await getRobots();
 
       expect(robots).toStrictEqual(mockRobots);
     });
@@ -22,9 +22,9 @@ describe("Given function getRobot from useRobotsApi custom hook", () => {
 
       const expectedError = new Error("Can't get any robot");
       const { result } = renderHook(() => useRobotsApi());
-      const { getRobot } = result.current;
+      const { getRobots } = result.current;
 
-      const error = getRobot();
+      const error = getRobots();
 
       expect(error).rejects.toThrowError(expectedError);
     });
